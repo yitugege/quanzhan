346fcc8fceb3112d84c18c4db907e2cd6e7f1ccb
yitugege <sam.zxj@qq.com>
2021-04-17 13:19:28 -0500

    添加功能

--- quanzhan.sh
+++ quanzhan.sh
@@ -4,7 +4,6 @@ source /home/yitu/tutorial-env/bin/activate
 #dir2=`cd /home/yitu/scrapy-redis/scrapy_redis_test/spiders/`
 #command=`nohup python -u run.py > 1.log 2>&1 &`
 quanzhan_pid=`ps -aux|grep -v 'grep'|grep -c 'quanzhan'`
-mercado_pid=`ps -aux|grep -v 'grep'|grep -c 'mercado'`
 if [ $quanzhan_pid -eq 0 ]
 then
   cd /home/yitu/quanzhan
@@ -12,13 +11,3 @@ then
   cd /home/yitu/quanzhan/mercado_redis/spiders/
   nohup python -u run.py > 1.log 2>&1 &
 fi
-
-if [ $mercado_pid -eq 0 ]
-then
-  cd /home/yitu/scrapy-redis/
-  git pull 
-  cd /home/yitu/scrapy-redis/scrapy_redis_test/spiders/
-  nohup python -u run.py > 1.log 2>&1 &
-fi
-
-
