9c82631b7747e61175fddd5ce1b2b66ac145ff18
yitugege <sam.zxj@qq.com>
2022-05-17 00:32:22 -0500

    修改功能

--- mercado_redis/spiders/dange.py
+++ mercado_redis/spiders/dange.py
@@ -14,12 +14,13 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            # 'https://www.mercadolibre.com.mx/guitarra-electrica-yamaha-pac012100-series-112v-de-aliso-vintage-white-brillante-con-diapason-de-palo-de-rosa/p/MLM17517052',
+             'https://www.mercadolibre.com.mx/guitarra-electrica-yamaha-pac012100-series-112v-de-aliso-vintage-white-brillante-con-diapason-de-palo-de-rosa/p/MLM17517052',
              'https://articulo.mercadolibre.com.mx/MLM-1300005017-4-altavoz-motorola-g7-power-g7-play-g8-power-original-4pzs-_JM',
-            # 'https://www.mercadolibre.com.mx/sniper-elite-iii-ultimate-edition-505-games-xbox-one-fisico/p/MLM6165822#reco_item_pos=13&reco_backend=machinalis-pdp-v2p&reco_backend_type=low_level&reco_client=pdp-v2p&reco_id=15d78e2e-7533-4c4f-908e-822347a7aa9f',
-            # 'https://articulo.mercadolibre.com.mx/MLM-1367897814-auricular-con-flex-compatible-con-iphone-x-_JM#position=1&search_layout=stack&type=pad&tracking_id=8407e428-916d-4214-84f6-5d5cd4caf4c4&is_advertising=true&ad_domain=VQCATCORE_LST&ad_position=1&ad_click_id=ZDBjOTQzNTgtMjcwMC00ZjhkLWIyMTYtNmY1NDEwNmExZDQ4',
-            # 'https://www.mercadolivre.com.br/processador-gamer-amd-ryzen-5-3600-100-100000031box-de-6-nucleos-e-42ghz-de-frequncia/p/MLB15143240?hide_psmb=true',
-            # 'https://www.mercadolivre.com.br/processador-gamer-amd-ryzen-5-3600x-100-100000022box-de-6-nucleos-e-44ghz-de-frequncia/p/MLB15080182'
+             'https://www.mercadolibre.com.mx/anteojos-de-sol-ray-ban-round-frank-legend-standard-con-marco-de-metal-color-polished-gold-lente-light-blue-degradada-varilla-polished-gold-de-metal-rb3857/p/MLM17380290',
+             'https://www.mercadolibre.com.mx/sniper-elite-iii-ultimate-edition-505-games-xbox-one-fisico/p/MLM6165822#reco_item_pos=13&reco_backend=machinalis-pdp-v2p&reco_backend_type=low_level&reco_client=pdp-v2p&reco_id=15d78e2e-7533-4c4f-908e-822347a7aa9f',
+             'https://articulo.mercadolibre.com.mx/MLM-1367897814-auricular-con-flex-compatible-con-iphone-x-_JM#position=1&search_layout=stack&type=pad&tracking_id=8407e428-916d-4214-84f6-5d5cd4caf4c4&is_advertising=true&ad_domain=VQCATCORE_LST&ad_position=1&ad_click_id=ZDBjOTQzNTgtMjcwMC00ZjhkLWIyMTYtNmY1NDEwNmExZDQ4',
+             'https://www.mercadolivre.com.br/processador-gamer-amd-ryzen-5-3600-100-100000031box-de-6-nucleos-e-42ghz-de-frequncia/p/MLB15143240?hide_psmb=true',
+             'https://www.mercadolivre.com.br/processador-gamer-amd-ryzen-5-3600x-100-100000022box-de-6-nucleos-e-44ghz-de-frequncia/p/MLB15080182'
             ]
         for url in urls:
             yield scrapy.Request(url=url,dont_filter=True,callback=self.parse)
@@ -36,15 +37,16 @@ class QuotesSpider(scrapy.Spider):
         #链接
         url = response.url
         #获取商品ID非空那么插入，否则抓取302之前的url获取id从数据库删除
-        id = re.findall(r"/M\w\w(\d+|-\d+|/)",url)
+        id = re.findall(r"/M\w\w(\d{7,}|-\d{7,}|/)",url)
+        print(id)
         #id = re.findall(r"\d{7,}",url)
         if  id != []:
             id = abs(int("".join([str(x) for x in id])))
         else:
             url=response.request.meta.get('redirect_urls')[0]
-            id = re.findall(r"/M\w\w(\d+|-\d+|/)",url)
+            id = re.findall(r"/M\w\w(\d{7,}|-\d{7,}|/)",url)
             id = abs(int("".join([str(x) for x in id])))
-        print(id)
+        
 
          #获取价格 没有价格删除连接
         price = response.xpath("//div[@class='ui-pdp-price mt-16 ui-pdp-price--size-large']/div[@class='ui-pdp-price__second-line']/span[@class='andes-money-amount ui-pdp-price__part andes-money-amount--cents-superscript andes-money-amount--compact']/span[@class='andes-money-amount__fraction']/text()").get()
